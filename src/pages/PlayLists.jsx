e9e9c12f32e97aad7fa971dd1ec8ce4bc139537f
Esteban Martinez <camilo.martinezmma@cun.edu.co>
2023-12-31 15:16:09 -0500

    v1.0.7

--- src/pages/PlayLists.jsx
+++ src/pages/PlayLists.jsx
@@ -1,5 +1,30 @@
+import { useEffect } from "react"
+import { axiosMusic } from "../utils/ConfigAxios"
+import { Header } from "../components/shared/Header"
+
 export const PlayLists = () => {
+
+  useEffect(() => {
+    axiosMusic
+      .get("/api/playlists/me")
+      .then(({ data }) => {
+        console.log(data)
+      })
+  }, [])
+
   return (
-    <div>PlayLists</div>
+    <section
+      className="bg-dark bg-[url('/bagraund/mancha-mobile.png')] md:bg-[url('/bagraund/mancha-desk.png')] bg-no-repeat bg-right-bottom text-white h-screen overflow-auto font-urbanist grid grid-rows-[auto_1fr] ">
+      {/* //? Encabezado */}
+      <Header />
+      {/* //? Contenido */}
+      <section
+        className="p-2 mt-12 max-w-[562px] mx-auto">
+        <main
+          className="bg-primary-dark p-8 px-4 rounded-3xl sm:px-16">
+            asdasd
+        </main>
+      </section>
+    </section>
   )
 }
