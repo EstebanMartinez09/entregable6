18a032097fdf224a68c9ad59b6ad3334021a4420
Esteban Martinez <camilo.martinezmma@cun.edu.co>
2024-01-01 15:59:01 -0500

    se arregla elemento de la page playlist

--- src/components/shared/Cassette.jsx
+++ src/components/shared/Cassette.jsx
@@ -11,22 +11,23 @@ export const Cassette = ({ playlist, index }) => {
         <div
             style={{
                 transform: `translateY(${index * 50}px)`,
+                
                 zIndex: hover ? 1 : 0,
             }}
-            className="absolute left-[calc(50%-119px)] w-[238px]">
+            className="absolute left-[calc(50%-119px)] w-[238px] h-[180px]">
                 {/* Link para ir al detail de la playlist */}
             <Link
                 onMouseEnter={() => setHover(true)}
                 onMouseLeave={() => setHover(false)}
-                className="relative"
+               
                 to={`/playlists/${playlist.id}`}>
                 <div
-                    className=" hover:-translate-y-[25px] transition-all">
+                    className="relative hover:-translate-y-[25px] transition-all w-full h-full">
                     <img
                         src="/images/frontalCaset.png"
                         alt="caset" />
                     <div
-                        className="absolute top-7 left-5 w-[200px] flex justify-between items-center p-1 bg-white rounded-md border-[2px] border-[#1C1C1C] ">
+                        className="absolute top-[8%] left-5 w-[200px] flex justify-between items-center p-1 bg-white rounded-md border-[2px] border-[#1C1C1C] ">
                         <p
                             className="text-black">
                             {playlist.title}
